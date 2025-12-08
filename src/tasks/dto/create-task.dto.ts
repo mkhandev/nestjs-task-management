472a1b1aca2e917a422da089d2262660a7a0057a
Md. Abdullah Al Mahmud Khan <mahmudkhan.sumon@gmail.com>
2025-12-08 23:48:55 +0600

    add validation

--- src/tasks/dto/create-task.dto.ts
+++ src/tasks/dto/create-task.dto.ts
@@ -1,7 +1,13 @@
+import { IsEnum, IsNotEmpty } from 'class-validator';
 import { TaskStatus } from 'src/tasks/task-status.enum';
 
 export class CreateTaskDto {
+  @IsNotEmpty()
   title: string;
+
+  @IsNotEmpty()
   description: string;
-  status: TaskStatus;
+
+  @IsEnum(TaskStatus)
+  status?: TaskStatus;
 }
